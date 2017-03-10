98b640b2ddc29a9043acfca252468f121f94d5ba
Ismaël Mejía <iemejia@gmail.com>
2017-03-10 01:14:16 +0100

    Improve docker-entrypoint after shellcheck

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-################################################################################
+###############################################################################
 #  Licensed to the Apache Software Foundation (ASF) under one
 #  or more contributor license agreements.  See the NOTICE file
 #  distributed with this work for additional information
@@ -16,31 +16,36 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 # limitations under the License.
-################################################################################
+###############################################################################
 
-### If unspecified, the hostname of the container is taken as the JobManager address
+# If unspecified, the hostname of the container is taken as the JobManager address
 JOB_MANAGER_RPC_ADDRESS=${JOB_MANAGER_RPC_ADDRESS:-$(hostname -f)}
-###
 
-if [ "$1" == "--help" -o "$1" == "-h" ]; then
-    echo "Usage: $(basename $0) (jobmanager|taskmanager)"
+if [ "$1" = "help" ]; then
+    echo "Usage: $(basename "$0") (jobmanager|taskmanager|local|help)"
     exit 0
-elif [ "$1" == "jobmanager" ]; then
+elif [ "$1" = "jobmanager" ]; then
     echo "Starting Job Manager"
-    sed -i -e "s/jobmanager.rpc.address: localhost/jobmanager.rpc.address: ${JOB_MANAGER_RPC_ADDRESS}/g" $FLINK_HOME/conf/flink-conf.yaml
+    sed -i -e "s/jobmanager.rpc.address: localhost/jobmanager.rpc.address: ${JOB_MANAGER_RPC_ADDRESS}/g" "$FLINK_HOME/conf/flink-conf.yaml"
 
-    echo "config file: " && grep '^[^\n#]' $FLINK_HOME/conf/flink-conf.yaml
-    $FLINK_HOME/bin/jobmanager.sh start cluster
+    echo "config file: " && grep '^[^\n#]' "$FLINK_HOME/conf/flink-conf.yaml"
+    "$FLINK_HOME/bin/jobmanager.sh" start cluster
 
     # prevent script from exiting
     tail -f /dev/null
-elif [ "$1" == "taskmanager" ]; then
-    sed -i -e "s/jobmanager.rpc.address: localhost/jobmanager.rpc.address: ${JOB_MANAGER_RPC_ADDRESS}/g" $FLINK_HOME/conf/flink-conf.yaml
-    sed -i -e "s/taskmanager.numberOfTaskSlots: 1/taskmanager.numberOfTaskSlots: $(grep -c ^processor /proc/cpuinfo)/g" $FLINK_HOME/conf/flink-conf.yaml
+elif [ "$1" = "taskmanager" ]; then
+    sed -i -e "s/jobmanager.rpc.address: localhost/jobmanager.rpc.address: ${JOB_MANAGER_RPC_ADDRESS}/g" "$FLINK_HOME/conf/flink-conf.yaml"
+    sed -i -e "s/taskmanager.numberOfTaskSlots: 1/taskmanager.numberOfTaskSlots: $(grep -c ^processor /proc/cpuinfo)/g" "$FLINK_HOME/conf/flink-conf.yaml"
 
     echo "Starting Task Manager"
-    echo "config file: " && grep '^[^\n#]' $FLINK_HOME/conf/flink-conf.yaml
-    $FLINK_HOME/bin/taskmanager.sh start
+    echo "config file: " && grep '^[^\n#]' "$FLINK_HOME/conf/flink-conf.yaml"
+    "$FLINK_HOME/bin/taskmanager.sh" start
+
+    # prevent script from exiting
+    tail -f /dev/null
+elif [ "$1" = "local" ]; then
+    echo "Starting local cluster"
+    "$FLINK_HOME/bin/start-local.sh"
 
     # prevent script from exiting
     tail -f /dev/null
