63b19a904fa8bfd1322f1d59fdb226c82b9186c7
Patrick Lucas <me@patricklucas.com>
2019-01-15 17:40:49 +0100

    Update Dockerfiles

--- 1.7/hadoop28-scala_2.12-alpine/docker-entrypoint.sh
+++ 1.7/hadoop28-scala_2.12-alpine/docker-entrypoint.sh
@@ -22,12 +22,15 @@
 JOB_MANAGER_RPC_ADDRESS=${JOB_MANAGER_RPC_ADDRESS:-$(hostname -f)}
 
 drop_privs_cmd() {
-    if [ -x /sbin/su-exec ]; then
+    if [ $(id -u) != 0 ]; then
+        # Don't need to drop privs if EUID != 0
+        return
+    elif [ -x /sbin/su-exec ]; then
         # Alpine
-        echo su-exec
+        echo su-exec flink
     else
         # Others
-        echo gosu
+        echo gosu flink
     fi
 }
 
@@ -42,7 +45,7 @@ elif [ "$1" = "jobmanager" ]; then
     echo "query.server.port: 6125" >> "$FLINK_HOME/conf/flink-conf.yaml"
 
     echo "config file: " && grep '^[^\n#]' "$FLINK_HOME/conf/flink-conf.yaml"
-    exec $(drop_privs_cmd) flink "$FLINK_HOME/bin/jobmanager.sh" start-foreground "$@"
+    exec $(drop_privs_cmd) "$FLINK_HOME/bin/jobmanager.sh" start-foreground "$@"
 elif [ "$1" = "taskmanager" ]; then
     TASK_MANAGER_NUMBER_OF_TASK_SLOTS=${TASK_MANAGER_NUMBER_OF_TASK_SLOTS:-$(grep -c ^processor /proc/cpuinfo)}
 
@@ -53,7 +56,7 @@ elif [ "$1" = "taskmanager" ]; then
 
     echo "Starting Task Manager"
     echo "config file: " && grep '^[^\n#]' "$FLINK_HOME/conf/flink-conf.yaml"
-    exec $(drop_privs_cmd) flink "$FLINK_HOME/bin/taskmanager.sh" start-foreground
+    exec $(drop_privs_cmd) "$FLINK_HOME/bin/taskmanager.sh" start-foreground
 fi
 
 exec "$@"
