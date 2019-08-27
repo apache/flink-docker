13afbf10fd39760cf4ee3138c248685c8cf5f922
Patrick Lucas <me@patricklucas.com>
2019-08-27 11:31:02 +0200

    Reject PRs that modify generated files

--- testing/run_travis_tests.sh
+++ testing/run_travis_tests.sh
@@ -11,6 +11,19 @@ fi
 
 BRANCH="$TRAVIS_BRANCH"
 
+if [ -n "$IS_PULL_REQUEST" ]; then
+  changed_files="$(git diff --name-only $BRANCH...HEAD)"
+
+  echo "Changed files in this pull request:"
+  echo "${changed_files}"
+  echo
+
+  if echo "${changed_files}" | grep -q '^[0-9]\+\.[0-9]\+/'; then
+    echo 'error: generated files in x.y/ directories should not be modified.'
+    exit 1
+  fi
+fi
+
 if [ -z "$IS_PULL_REQUEST" ] && [ "$BRANCH" = "master" ]; then
   # Test all images on master
   smoke_test_all_images
@@ -19,3 +32,5 @@ else
   # For pull requests and branches, test one image
   smoke_test_one_image
 fi
+
+# vim: et ts=2 sw=2
