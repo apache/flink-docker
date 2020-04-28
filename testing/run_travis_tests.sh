dfdb81f4ef8b86db0c0d6e2d62a6920765d64efd
Chesnay Schepler <chesnay@apache.org>
2020-04-28 14:19:55 +0200

    [FLINK-17439] Fork dev-master branch
    - remove .asf.yaml, we only need it in the master branch
    - remove generated Dockerfiles,we can now generate them during CI instead
    - remove README.md, the master README.md will contain all necessary information
    - remove stackbrew generators, only relevant for releases and will be retained on master
    - remove testing_lib.sh#smoke_test_one_image, now redundant
    - deduplicate testing_lib#sh#smoke_test_all_images and #smoke_test_non_root

--- testing/run_travis_tests.sh
+++ testing/run_travis_tests.sh
@@ -19,10 +19,11 @@ if [ -n "$IS_PULL_REQUEST" ]; then
   echo
 fi
 
-if [ -z "$IS_PULL_REQUEST" ] && [ "$BRANCH" = "master" ]; then
-  # Test all images on master
+./add-custom.sh -u "https://s3.amazonaws.com/flink-nightly/flink-1.11-SNAPSHOT-bin-hadoop2.tgz"
+
+if [ -z "$IS_PULL_REQUEST" ] && [ "$BRANCH" = "dev-master" ]; then
   smoke_test_all_images
-  smoke_test_non_root
+  smoke_test_one_image_non_root
 else
   # For pull requests and branches, test one image
   smoke_test_one_image
