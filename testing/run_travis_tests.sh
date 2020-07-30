fdf3d3c6aa1024fce20a7d49030c0df703e6562d
Robert Metzger <rmetzger@apache.org>
2020-07-30 08:24:02 +0200

    test all images

--- testing/run_travis_tests.sh
+++ testing/run_travis_tests.sh
@@ -11,24 +11,14 @@ fi
 
 BRANCH="$TRAVIS_BRANCH"
 
-function run_tests {
-	if [ -z "$IS_PULL_REQUEST" ] && [ "$BRANCH" = "dev-master" ]; then
-  smoke_test_all_images
-  smoke_test_one_image_non_root
-else
-  # For pull requests and branches, test one image
-  smoke_test_one_image
-fi
-}
-
-./add-custom.sh -u "https://s3.amazonaws.com/flink-nightly/flink-1.11-SNAPSHOT-bin-hadoop2.tgz"
-run_tests
-
-rm -r dev
+./add-custom.sh -u "https://s3.amazonaws.com/flink-nightly/flink-1.11-SNAPSHOT-bin-hadoop2.tgz" -n test-java8
 
 # test Flink with Java11 image as well
-./add-custom.sh -u "https://s3.amazonaws.com/flink-nightly/flink-1.11-SNAPSHOT-bin-hadoop2.tgz" -j 11
-run_tests
+./add-custom.sh -u "https://s3.amazonaws.com/flink-nightly/flink-1.11-SNAPSHOT-bin-hadoop2.tgz" -j 11 -n test-java11
+
+smoke_test_all_images
+smoke_test_one_image_non_root
+
 
 echo "Test successfully finished"
 
