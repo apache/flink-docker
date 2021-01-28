5d0a9d4b326dcc66190767a03447f03db7e84767
Xintong Song <tonysong820@gmail.com>
2021-01-28 17:46:47 +0800

    Add GPG key for 1.10.3 release

--- add-version.sh
+++ add-version.sh
@@ -89,6 +89,8 @@ elif [ "$flink_version" = "1.10.1" ]; then
     gpg_key="D8D3D42E84C753CA5F170BDF93C07902771AB743"
 elif [ "$flink_version" = "1.10.2" ]; then
     gpg_key="C63E230EFFF519A5BBF2C9AE6767487CD505859C"
+elif [ "$flink_version" = "1.10.3" ]; then
+    gpg_key="F8E419AA0B60C28879E876859DFF40967ABFC5A4"
 else
     error "Missing GPG key ID for this release"
 fi
