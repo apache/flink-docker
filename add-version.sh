b3fd17d82f9f206d3f4f871b1d81686ae909b909
Chesnay Schepler <chesnay@apache.org>
2021-12-14 13:24:48 +0100

    Add GPG key for 1.12.6 release

--- add-version.sh
+++ add-version.sh
@@ -100,6 +100,8 @@ elif [ "$flink_version" = "1.12.4" ]; then
     gpg_key="A53C7D531C6889386EB6D94E476DAA5D1FF08189"
 elif [ "$flink_version" = "1.12.5" ]; then
     gpg_key="9545FBA24D2225795DBAAF8EFBB83C0A4FFB9CA8"
+elif [ "$flink_version" = "1.12.6" ]; then
+    gpg_key="19F2195E1B4816D765A2C324C2EED7B111D464BA"
 else
     error "Missing GPG key ID for this release"
 fi
