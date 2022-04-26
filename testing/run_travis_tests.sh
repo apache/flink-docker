c91abe6842f50e12394b8696a64a8c5145e8a083
Chesnay Schepler <chesnay@apache.org>
2022-04-26 11:45:28 +0200

    [FLINK-27413] Drop Scala 2.11 support

--- testing/run_travis_tests.sh
+++ testing/run_travis_tests.sh
@@ -13,10 +13,10 @@ BRANCH="$TRAVIS_BRANCH"
 
 test_docker_entrypoint
 
-./add-custom.sh -u "https://s3.amazonaws.com/flink-nightly/flink-1.15-SNAPSHOT-bin-scala_2.11.tgz" -j 8 -n test-java8
+./add-custom.sh -u "https://s3.amazonaws.com/flink-nightly/flink-1.15-SNAPSHOT-bin-scala_2.12.tgz" -j 8 -n test-java8
 
 # test Flink with Java11 image as well
-./add-custom.sh -u "https://s3.amazonaws.com/flink-nightly/flink-1.15-SNAPSHOT-bin-scala_2.11.tgz" -j 11 -n test-java11
+./add-custom.sh -u "https://s3.amazonaws.com/flink-nightly/flink-1.15-SNAPSHOT-bin-scala_2.12.tgz" -j 11 -n test-java11
 
 smoke_test_all_images
 smoke_test_one_image_non_root
