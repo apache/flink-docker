da24eec13b88a8319384527f28cc75ca3f421478
Matthias Pohl <matthias.pohl@aiven.io>
2023-03-23 11:59:58 +0100

    [FLINK-31566][docker] Add GPG key for 1.17.0 release

--- add-version.sh
+++ add-version.sh
@@ -98,6 +98,8 @@ elif [ "$flink_version" = "1.15.0" ]; then
     gpg_key="CBE82BEFD827B08AFA843977EDBF922A7BC84897"
 elif [ "$flink_version" = "1.16.0" ]; then
     gpg_key="EB3FE0FA3282FAF822E434AE3C2C9FFB59DF9F3E"
+elif [ "$flink_version" = "1.17.0" ]; then
+    gpg_key="A1BD477F79D036D2C30CA7DBCA8AEEC2F6EB040B"
 else
     error "Missing GPG key ID for this release"
 fi
