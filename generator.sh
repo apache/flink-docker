0122df745f0b59a793b400f0150d57fee479a38a
Daniel Packard <daniel.packard@hey.com>
2023-06-08 13:24:26 -0500

    Update generator.sh
    Correct argument reference for scala_version from $6 to $7
    
    Co-authored-by: Chesnay Schepler <chesnay@apache.org>

--- generator.sh
+++ generator.sh
@@ -13,7 +13,7 @@ function generateDockerfile {
     gpg_key=$4
     check_gpg=$5
     java_version=$6
-    scala_version=$6
+    scala_version=$7
     source_variant=$8
 
     flink_url_file_path=flink/flink-${flink_version}/flink-${flink_version}-bin-scala_${scala_version}.tgz
