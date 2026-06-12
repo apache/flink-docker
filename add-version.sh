6ea63a3e2a51ffb14a161d0cc06c9adcc6751e66
云岩 <zhouyunfeng.zyf@alibaba-inc.com>
2026-06-12 11:45:36 +0800

    Add GPG key for 1.20.5 release

--- add-version.sh
+++ add-version.sh
@@ -112,6 +112,8 @@ elif [ "$flink_version" = "1.20.3" ]; then
     gpg_key="5EE2CDBE52BDDB9CCB1C63B6E6BAF9CFABD4ED71"
 elif [ "$flink_version" = "1.20.4" ]; then
     gpg_key="F7529FAE24811A5C0DF3CA741596BBF0726835D8"
+elif [ "$flink_version" = "1.20.5" ]; then
+    gpg_key="2EC0F1E82EA872E30FE4EDEE7C83478D4985D451"
 else
     error "Missing GPG key ID for this release"
 fi
