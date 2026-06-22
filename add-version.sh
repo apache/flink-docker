891352f55a41c6a95956995888f7817dca70c441
David Anderson <david@alpinegizmo.com>
2026-06-22 15:47:34 -0700

    Add GPG key for 2.3.0 release

--- add-version.sh
+++ add-version.sh
@@ -114,6 +114,8 @@ elif [ "$flink_version" = "2.1.0" ]; then
     gpg_key="7A14EF9AD986EF0D56B2E73F6AF817E6C59EC690"
 elif [ "$flink_version" = "2.2.0" ]; then
     gpg_key="7BC9FA3EBE7E3DC7CD0EA0454C09617EAF241D76"
+elif [ "$flink_version" = "2.3.0" ]; then
+    gpg_key="E11F792053638C56C98ACFADB5B638DCA5F94E4A"
 else
     error "Missing GPG key ID for this release"
 fi
